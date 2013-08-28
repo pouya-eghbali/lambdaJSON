4067fe9050ae96fa1894008360dfc980e43dbca4
Pooya Eghbali <persian.writer@gmail.com>
2013-08-28 17:18:03 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+===========
+lambdaJSON
+===========
+
+lambdaJSON lets you serialize python standard library objects
+with <json>.
+Typical usage::
+
+    #!/usr/bin/env python
+
+    import lambdaJSON
+
+    mySerialisedObject = lambdaJSON.serialize({(1,2,3):{b'lambda':[b'json',1,3]}, 45: 'test'})
+	
+	myDeserialisedObject = lambdaJSON.deserialize(mySerialisedObject)
+
+Currently Supported Types
+=========================
+
+This types are covered in this version:
+
+1. Bytes
+2. Tuples
+3. Dicts (With Number, Tuple, String and Byte keys)
+4. other json supported types
+
+Github project page: https://github.com/pooya-eghbali/lambdaJSON
---
+++ lambdaJSON.py
@@ -0,0 +1,35 @@
+import json
+
+flatten = lambda obj:   (isinstance(obj, bytes) and
+                        (b'bytes://'+obj).decode('utf8') or
+                        isinstance(obj, tuple) and
+                        'tuple://'+str(obj) or
+                        isinstance(obj, list) and
+                        [flatten(i) for i in obj] or
+                        isinstance(obj, dict) and
+                        {(lambda x: isinstance(x, (int, float, complex)) and
+                        str(type(x))[8:-2]+'://'+str(x) or
+                        flatten(x))
+                        (i):flatten(obj[i]) for i in obj} or
+                        obj)
+
+restore = lambda obj:   (isinstance(obj, str) and
+                        (lambda x: x.startswith('bytes://') and
+                        bytes(x[8:], encoding = 'utf8') or
+                        x.startswith('int://') and
+                        int(x[6:]) or
+                        x.startswith('float://') and
+                        float(x[8:]) or
+                        x.startswith('complex://') and
+                        complex(x[10:]) or
+                        x.startswith('tuple://') and
+                        eval(x[8:]) or x)(obj) or
+                        isinstance(obj, list) and
+                        [restore(i) for i in obj] or
+                        isinstance(obj, dict) and
+                        {restore(i):restore(obj[i]) for i in obj} or
+                        obj)
+
+serialize   = lambda obj: json.dumps(flatten(obj))
+deserialize = lambda obj: restore(json.loads(obj))
+
