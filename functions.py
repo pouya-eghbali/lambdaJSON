f8671f8446b7f2101659e9098d9bc85ee8103928
pooya-eghbali <persian.writer@gmail.com>
2013-09-03 12:42:52 -0700

    Better code!

--- functions.py
+++ functions.py
@@ -1,25 +1,27 @@
-try:
-    freezef = lambda function:     {'co_argcount': function.__code__.co_argcount,
-                                   'co_kwonlyargcount': function.__code__.co_kwonlyargcount,
-                                   'co_nlocals': function.__code__.co_nlocals,
-                                   'co_stacksize': function.__code__.co_stacksize,
-                                   'co_flags': function.__code__.co_flags,
-                                   'co_code': function.__code__.co_code,
-                                   'co_consts': function.__code__.co_consts,
-                                   'co_names': function.__code__.co_names,
-                                   'co_varnames': function.__code__.co_varnames,
-                                   'co_filename': function.__code__.co_filename,
-                                   'co_name': function.__code__.co_name,
-                                   'co_firstlineno': function.__code__.co_firstlineno,
-                                   'co_lnotab': function.__code__.co_lnotab}
+freezef = lambda function:         (lambda code = (hasattr(function, '__code__')
+                                   and function.__code__ or function.func_code):
+                                   {'co_argcount': code.co_argcount,
+                                   'co_kwonlyargcount': (
+                                   hasattr(code,'co_kwonlyargcount')
+                                   and code.co_kwonlyargcount),
+                                   'co_nlocals': code.co_nlocals,
+                                   'co_stacksize': code.co_stacksize,
+                                   'co_flags': code.co_flags,
+                                   'co_code': code.co_code,
+                                   'co_consts': code.co_consts,
+                                   'co_names': code.co_names,
+                                   'co_varnames': code.co_varnames,
+                                   'co_filename': code.co_filename,
+                                   'co_name': code.co_name,
+                                   'co_firstlineno': code.co_firstlineno,
+                                   'co_lnotab': code.co_lnotab})()
 
-    defreezef = lambda co_dict, globs:\
+defreezef = lambda co_dict, globs:\
                                    eval("""(lambda lambdaJSON_globs = globs,
                                    co_dict = co_dict: (lambda %s:
                                    (type(lambda: None)(
                                    type((lambda: None).__code__)(
-                                   co_dict['co_argcount'],
-                                   co_dict['co_kwonlyargcount'],
+                                   co_dict['co_argcount'],%s
                                    co_dict['co_nlocals'],
                                    co_dict['co_stacksize'],
                                    co_dict['co_flags'],
@@ -32,39 +34,9 @@ try:
                                    co_dict['co_firstlineno'],
                                    co_dict['co_lnotab']),
                                    lambdaJSON_globs(),'')(%s))))()"""%
-                                   ((','.join(['%s'%a for a
-                                   in co_dict['co_varnames']]),)*2))
-except:
-    freezef = lambda function:     {'co_argcount': function.func_code.co_argcount,
-                                   'co_nlocals': function.func_code.co_nlocals,
-                                   'co_stacksize': function.func_code.co_stacksize,
-                                   'co_flags': function.func_code.co_flags,
-                                   'co_code': function.func_code.co_code,
-                                   'co_consts': function.func_code.co_consts,
-                                   'co_names': function.func_code.co_names,
-                                   'co_varnames': function.func_code.co_varnames,
-                                   'co_filename': function.func_code.co_filename,
-                                   'co_name': function.func_code.co_name,
-                                   'co_firstlineno': function.func_code.co_firstlineno,
-                                   'co_lnotab': function.func_code.co_lnotab}
-
-    defreezef = lambda co_dict, globs:\
-                                   eval("""(lambda lambdaJSON_globs = globs,
-                                   co_dict = co_dict: (lambda %s:
-                                   (type(lambda: None)(
-                                   type((lambda: None).__code__)(
-                                   co_dict['co_argcount'],
-                                   co_dict['co_nlocals'],
-                                   co_dict['co_stacksize'],
-                                   co_dict['co_flags'],
-                                   co_dict['co_code'],
-                                   co_dict['co_consts'],
-                                   co_dict['co_names'],
-                                   co_dict['co_varnames'],
-                                   co_dict['co_filename'],
-                                   co_dict['co_name'],
-                                   co_dict['co_firstlineno'],
-                                   co_dict['co_lnotab']),
-                                   lambdaJSON_globs(),'')(%s))))()"""%
-                                   ((','.join(['%s'%a for a
-                                   in co_dict['co_varnames']]),)*2))
+                                   (','.join(['%s'%a for a
+                                   in co_dict['co_varnames']]),
+                                   (co_dict['co_kwonlyargcount'] is False and ' '
+                                   or "co_dict['co_kwonlyargcount'],"),
+                                   ','.join(['%s'%a for a
+                                   in co_dict['co_varnames']])))
